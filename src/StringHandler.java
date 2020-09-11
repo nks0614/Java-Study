b7c860c951fb3427b90ed120c33a510137379dee
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-09-11 10:49:15 +0900

    [ADD] String Handler

--- src/StringHandler.java
+++ src/StringHandler.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class StringHandler {
-    
+    //https://programmers.co.kr/learn/courses/30/lessons/12918
+    //programmers 문자열 다루기 기본
+    public static void main(String[] args) {
+        boolean answer = true;
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        for(int i = 0; i < s.length(); i++){
+            char temp = s.charAt(i);
+
+            if(Character.isDigit(temp) == false){
+                answer = false;
+            }
+        }
+
+        System.out.println(answer);
+
+
+    }
 }
