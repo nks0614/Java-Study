b95f06fa72c02e5dbb11245c7ad137ba1489fdbc
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-10-12 09:26:21 +0900

    [ADD] X term N

---
+++ src/XtermN.java
@@ -0,0 +1,23 @@
+public class XtermN {
+
+    public static void main(String[] args) {
+
+        long[] answer = solution(-4,2);
+
+        for(int i = 0; i < answer.length; i++){
+            System.out.println(answer[i]);
+        }
+
+    }
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        for(int i = 0; i < n; i++){
+            answer[i] = (long) x * (i+1);
+        }
+
+        return answer;
+    }
+
+}
