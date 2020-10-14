3cf2a6ea02702d212901d599f4f59a868033ee56
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-10-14 14:44:29 +0900

    [ADD] Collatz Conjecture

---
+++ src/CollatzConjecture.java
@@ -0,0 +1,28 @@
+public class CollatzConjecture {
+
+    public static void main(String[] args) {
+        System.out.println(solution(626331));
+    }
+
+    public static int solution(int num) {
+        int answer = 0;
+
+        while(num != 1){
+            if(answer >= 500){
+                answer = -1;
+                break;
+            }
+
+            if(num % 2 == 0){
+                num /= 2;
+            }
+            else{
+                num = num * 3 + 1;
+            }
+            ++answer;
+        }
+
+        return answer;
+    }
+
+}
