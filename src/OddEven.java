daff778dac206d844f8922b721a9b1c5208a5608
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-10-19 08:42:50 +0900

    [ADD] Odd Even

---
+++ src/OddEven.java
@@ -0,0 +1,12 @@
+public class OddEven {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+    }
+
+    public static String solution(int num) {
+
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+}
