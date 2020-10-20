e6f3f8d4ca9956304d245ed91efa83c6a696dd37
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-10-20 09:33:13 +0900

    [ADD] Add Digit

---
+++ src/AddDigit.java
@@ -0,0 +1,20 @@
+public class AddDigit {
+
+    public static void main(String[] args) {
+        System.out.println(solution(987));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        while(n >= 10){
+            answer += n % 10;
+            n /= 10;
+        }
+
+        answer += n;
+
+        return answer;
+    }
+
+}
