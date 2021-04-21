4152a7fa8e8969dbe08b10159a45b22b06b20488
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2021-04-21 12:22:59 +0900

    [UPDATE] Date getHour is deprecated, use Calendar

--- src/concept/DateTest.java
+++ src/concept/DateTest.java
@@ -1,5 +1,6 @@
 package concept;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateTest {
@@ -16,6 +17,21 @@ public class DateTest {
         System.out.println(twoNumber(today.getDate()));
 
         System.out.println(today.getHours());
+
+        Calendar c = Calendar.getInstance();
+
+        System.out.println(c.get(Calendar.HOUR));
+        System.out.println(c.get(Calendar.HOUR_OF_DAY));
+
+        int hour = 18;
+
+        if(hour < 8 || hour > 19){
+            System.out.println("아침");
+        } else if(hour >= 8 && hour < 13){
+            System.out.println("점심");
+        } else {
+            System.out.println("저녁");
+        }
     }
 
     public static String twoNumber(int num) {
